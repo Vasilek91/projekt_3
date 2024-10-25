05e7fe3ef00755d718c06fa032cf0d5297712bf5
Vasilek91 <Hornstr@seznam.cz>
2024-10-25 18:03:28 +0200

    Update web_scraper.py

--- web_scraper.py
+++ web_scraper.py
@@ -54,7 +54,7 @@ def kontrola_nazvu_souboru(soubor):
         print(f'\nUvedl jsi nesprávný formát výstupního souboru. Uveď prosím název s koncovkou .csv.\n')
         sys.exit(1)
 
-# Funkce pro zápis výsledků do souboru
+# Zápis výsledků do souboru
 def zapis_vysledku(soubor, vysledky):
     zahlavi = vysledky[0].keys()
 
@@ -65,18 +65,47 @@ def zapis_vysledku(soubor, vysledky):
         zapisovac.writerows(vysledky)
     print(f"\n\nData byla uložena do souboru {soubor}.\n")
 
-# Zpracování stránky
-def zpracovani_stranky(adresa, prvni_cast_adresy):
-    vysledky = []
-
+def stahni_stranku(adresa):
     html_adresy = r.get(adresa).text
-    parsed_html = bs(html_adresy, 'html.parser')
+    parsed_html = bs(html_adresy, 'html.parser')    
 
-    # Zpracování HTML
-    td_tagy = parsed_html.find_all('td', {'class': 'cislo'})
-    nazev_obce = parsed_html.find_all('td', {'class': 'overflow_name'})
 
-    # Počítadlo okrsků pro zpětnou vazbu
+# Zpracování okrsku
+def zpracuj_okrsek(td, obec, prvni_cast_adresy):
+    a_tag = td.find('a')
+    nazev_okrsku = obec.text
+    cislo_okrsku = td.text
+
+    if a_tag:
+        href = a_tag.get('href')
+        if href:
+            odkazy_okrsku = urljoin(prvni_cast_adresy, href)
+            adresa_okrsku = r.get(odkazy_okrsku).text
+            parsed_okrsku = bs(adresa_okrsku, 'html.parser')
+            tabulka_okrsku = parsed_okrsku.find_all('td', {'headers': re.compile('^sa(2|3|6)$')})
+
+            volici_okrsku, obalky_okrsku, platne_hlasy = [td.text for td in tabulka_okrsku]
+            tabulka_stran = parsed_okrsku.find_all('td', {'headers': re.compile(r'^t\d+sb(2|3)$')})
+
+            vysledek = {
+                'cislo_okrsku': cislo_okrsku,
+                'nazev_okrsku': nazev_okrsku,
+                'volici': volici_okrsku,
+                'vydane_obalky': obalky_okrsku,
+                'platne_hlasy': platne_hlasy
+            }
+
+            for i in range(0, len(tabulka_stran), 2):
+                strana = tabulka_stran[i].text.strip()
+                hlasy = tabulka_stran[i + 1].text.strip()
+                vysledek[strana] = hlasy
+
+            return vysledek
+    return None
+
+# Zpracování okrksů do seznamu
+def zpracuj_vsechny_okrsky(td_tagy, nazev_obce, prvni_cast_adresy):
+    vysledky = []
     total_okrsky = len(td_tagy)
 
     for index, (td, obec) in enumerate(zip(td_tagy, nazev_obce), 1):
@@ -84,36 +113,24 @@ def zpracovani_stranky(adresa, prvni_cast_adresy):
         percent_done = (index / total_okrsky) * 100
         print(f"\rZpracováno {int(percent_done)} % okrsků", end='', flush=True)
 
-        a_tag = td.find('a')
-        nazev_okrsku = obec.text
-        cislo_okrsku = td.text
-
-        if a_tag:
-            href = a_tag.get('href')
-            if href:
-                odkazy_okrsku = urljoin(prvni_cast_adresy, href)
-                adresa_okrsku = r.get(odkazy_okrsku).text
-                parsed_okrsku = bs(adresa_okrsku, 'html.parser')
-                tabulka_okrsku = parsed_okrsku.find_all('td', {'headers': re.compile('^sa(2|3|6)$')})
-
-                volici_okrsku, obalky_okrsku, platne_hlasy = [td.text for td in tabulka_okrsku]
-                tabulka_stran = parsed_okrsku.find_all('td', {'headers': re.compile(r'^t\d+sb(2|3)$')})
-
-                vysledek = {
-                    'cislo_okrsku': cislo_okrsku,
-                    'nazev_okrsku': nazev_okrsku,
-                    'volici': volici_okrsku,
-                    'vydane_obalky': obalky_okrsku,
-                    'platne_hlasy': platne_hlasy
-                }
-
-                for i in range(0, len(tabulka_stran), 2):
-                    strana = tabulka_stran[i].text.strip()
-                    hlasy = tabulka_stran[i + 1].text.strip()
-                    vysledek[strana] = hlasy
-
-                vysledky.append(vysledek)             
+        vysledek = zpracuj_okrsek(td, obec, prvni_cast_adresy)
+        if vysledek:
+            vysledky.append(vysledek)
+    
     return vysledky
+
+# Zpracuje zadanou adresu
+def zpracovani_stranky(adresa, prvni_cast_adresy):
+    html_adresy = r.get(adresa).text
+    parsed_html = bs(html_adresy, 'html.parser')
+
+    # Zpracování HTML
+    td_tagy = parsed_html.find_all('td', {'class': 'cislo'})
+    nazev_obce = parsed_html.find_all('td', {'class': 'overflow_name'})
+
+    vysledky = zpracuj_vsechny_okrsky(td_tagy, nazev_obce, prvni_cast_adresy)
+    return vysledky
+
 
 # Kontrola vstupů
 zkontroluj_adresu_a_obsah(adresa)
